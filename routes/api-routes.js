547e06dd2a33d39ce551093f2595c0683aa05644
Kamshat Mussina <mussinak777@gmail.com>
2020-01-27 21:33:24 -0700

    added api and html routes, added function createworkout

---
+++ routes/api-routes.js
@@ -0,0 +1,48 @@
+// const router = require("express")
+const db = require("../models");
+
+module.exports = function (app) {
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate
+  (req.params.id,
+  {
+    $push: {exercises: req.body}
+  })
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+});
+
+app.post("/api/workouts", ({body}, res) => {
+  db.Workout.create(body)
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch(({ message }) => {
+    res.json(message)
+});
+})
+
+app.get("/api/workouts/range", (req, res) => {
+ db.Workout.find({})
+ .then(dbWorkout => {
+   res.json(dbWorkout)
+ })
+ .catch(err => {
+   res.json(err)
+ })
+});
+}
